635bde44335a6054d868dc5d897d8beb4989b0a7
rgaponov <rgaponov@omnicomm.ru>
2018-07-12 21:19:51 +0300

    создал структуру

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,4 +1,16 @@
 package entity;
 
+import java.sql.Timestamp;
+
 public class User {
+
+    private long id;
+
+    private String name;
+
+    private String email;
+
+    private int age;
+
+    private Timestamp createdDate;
 }
